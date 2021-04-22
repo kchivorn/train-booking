0dd5cef6ddff50ca6fa59a5f1c054e7cdda88c2d
kchivorn <kouch.chivorn@gmail.com>
2021-04-23 00:54:31 +0700

    Add association between models

--- app/models/departure.rb
+++ app/models/departure.rb
@@ -1,2 +1,18 @@
 class Departure < ApplicationRecord
+  LATE_TIME_START = 22
+  LATE_TIME_END = 6
+
+  has_many :tickets
+
+  scope :available, -> { where('departure_time > ? and seats_available > 0', 2.hours.from_now) }
+
+  def late
+    local_time = arrival_time.localtime - arrival_time.localtime.at_beginning_of_day
+
+    local_time >= LATE_TIME_START.hours || local_time <= LATE_TIME_END.hours
+  end
+
+  def cost_in_dollars
+    format('%.2f', (cost_in_cents / 100.0))
+  end
 end
