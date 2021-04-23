d92a76c4a5555bede591e0273d5b3747a4799cbd
kchivorn <kouch.chivorn@gmail.com>
2021-04-23 11:01:44 +0700

    remove unused comments

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,6 +1,4 @@
 class MainController < ApplicationController
-  # skip_before_action :set_current_user, only: %i[index]
-
   def index
     @departures = Departure.available
                            .paginate(page: params[:page], per_page: 10)
